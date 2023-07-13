6b487d222fa069905cc67d4aec99b4cef934a423
Seongeun Yu <s3ich4n@gmail.com>
2023-07-13 15:51:57 +0900

    feat: 6장 학습완료

--- nest-tutorials/src/app.module.ts
+++ nest-tutorials/src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { EmailModule } from './email/email.module';
+import { ConfigModule } from '@nestjs/config';
+import { validationSchema } from './config/validationSchema';
+import emailConfig from './config/emailConfig';
 
 @Module({
-  imports: [UsersModule, EmailModule],
+  imports: [
+    UsersModule,
+    EmailModule,
+    ConfigModule.forRoot({
+      envFilePath: [`${__dirname}/config/env/.env.${process.env.NODE_ENV}`],
+      load: [emailConfig],
+      isGlobal: true,
+      validationSchema,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
